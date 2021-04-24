c09eee954011c7add4e13ca76ac86523c0b25022
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2021-04-23 21:03:01 -0400

    Sus

---
+++ Items/Start.cs
@@ -0,0 +1,58 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CmdsMod;
+
+
+namespace CmdsMod.Items
+{
+	public class Start : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("CelestialBlade"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Your Sus");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.useTime = 45;
+			item.useAnimation = 45;
+			item.useStyle = ItemUseStyleID.HoldingUp;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.scale = 1.5f;
+			item.consumable = false;
+			item.maxStack = 1;
+		}
+
+		public override void AddRecipes()
+		{
+			//ModRecipe recipe = new ModRecipe(mod);
+			//recipe.AddIngredient(ItemID.DirtBlock, 32);
+			//recipe.AddTile(TileID.Anvils);
+			//recipe.SetResult(this);
+			//recipe.AddRecipe();
+		}
+		public override bool CanUseItem(Player player)
+		{
+			// "player.ZoneUnderworldHeight" could also be written as "player.position.Y / 16f > Main.maxTilesY - 200"
+			return !NPC.AnyNPCs(ModContent.NPCType<NPCs.Sus>());
+		}
+		public override bool UseItem(Player player)
+		{
+			Main.PlaySound(SoundID.Roar, player.Center, 0);
+			NPC.SpawnOnPlayer(player.whoAmI, ModContent.NPCType<NPCs.Sus>());
+			return true;
+		}
+		public override void OnConsumeItem(Player player)
+		{
+
+
+		}
+	}
+}
